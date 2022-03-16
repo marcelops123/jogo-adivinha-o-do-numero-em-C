2f17de698a5e6250c7721e2f2634516a0e3901b2
Marcelin <marceloaugusto10123@gmail.com>
2022-03-16 17:29:19 -0300

    update

--- Jogo adivinhar numero aleatorio/teste.c
+++ Jogo adivinhar numero aleatorio/teste.c
@@ -10,18 +10,21 @@ int main(void)
     printf("---------------------------- \n");
     printf("-----Seja bem vindo!-----\n ");
     printf("---------------------------- \n");
-    printf("Para comecarmos, por favor digite seu nome:\n");
+    printf("Para comecar, por favor digite seu nome:\n");
     scanf("%[^\n]s", nome);
     printf("Ola,  %s\n", nome);
 
     numero_aleatorio = srand() % 100;
-    printf("Foi gerado um numero aleatorio, tente adivinhar qual e \n");
+    printf("------------------------------------------------------- \n");
+    printf("Foi gerado um numero aleatorio, tente adivinhar qual e: \n");
+    printf("------------------------------------------------------- \n");
     while (numero_aleatorio != numero_digitado)
     {
         scanf("%d", &numero_digitado);
+
         if (numero_aleatorio != numero_digitado)
             printf("Numero errado, tente outro : \n");
-
+            printf("---------------------------- \n");
         if (numero_aleatorio < numero_digitado)
 
             printf("O numero gerado e menor que o numero que vc digitou...");
@@ -33,6 +36,8 @@ int main(void)
     if (numero_digitado == numero_aleatorio)
     {
         printf("Parabens!! Voce acertou! \n");
+        printf("Fim de jogo! Obrigado por participar: \n");
+        printf("Me siga no github para maiores projetos: marcelops123 \n");
     }
-    printf("O numero gerado era: %d ", numero_aleatorio);
+   
 }
